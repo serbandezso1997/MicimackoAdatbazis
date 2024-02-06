eb8978a0caabd2c716201f58e89d7b2ba13cfbc2
Serbán Dezső <root>
2024-02-06 21:35:28 +0100

    a szereplők adatbázishoz való hozzáadása.

--- MicimackoAdatbazis/Program.cs
+++ MicimackoAdatbazis/Program.cs
@@ -20,13 +20,22 @@ namespace MicimackoAdatbazis
             //Adatok();
             utasitasokLista=SqlUtasitasok();
             SQLiteCommand parancs = new SQLiteCommand(kapcsolat);
+
+            // Elsőnek törlöm az adatbázis korábbi tartalmát
+            string torloUtasitas = "DELETE FROM `Szereplok`;";
+            parancs.CommandText = torloUtasitas;
+            parancs.ExecuteNonQuery();
             for (int i = 0; i < utasitasokLista.Count; i++) 
             {
                 parancs.CommandText = utasitasokLista[i];
+                if (utasitasokLista[i].Contains("SELECT"))
+                {
+
+                }
                 parancs.ExecuteNonQuery();
                 
             }
-
+            
             string lekerdez = "SELECT * FROM `Szereplok`;";
             parancs.CommandText = lekerdez;
             SQLiteDataReader sQLiteDataReader = parancs.ExecuteReader();
@@ -41,15 +50,19 @@ namespace MicimackoAdatbazis
                 lekerdezett.Add(sQLiteDataReader.GetValue(index++).ToString());
                 //index++;
             }
+            
             kapcsolat.Close();
             Console.WriteLine("A kinyert adatok:");
-            foreach (string s in lekerdezett)
-            {
-                Console.WriteLine(s);
-            }
+            
+                        foreach (string s in lekerdezett)
+                        {
+                            Console.WriteLine(s);
+                        }
+            
+            //Console.WriteLine("a lekérdezett első eleme:" +lekerdezett[0]);
 
         }
-
+        /*
         //adatok legenerálása
         static void Adatok()
         {
@@ -113,6 +126,72 @@ namespace MicimackoAdatbazis
                 sor++;
             }
         }
+        */
+
+        static List <string> Adatok()
+        {
+            List <string> kiMitSzeret = new List <string>();
+            //a mese szereplői (9fő)
+            string[] szereplok = new string[] {
+                "Micimackó",
+                "Malacka",
+                "Füles",
+                "Nyuszi",
+                "Róbert Gida",
+                "Kanga",
+                "Zsebibaba",
+                "Bagoly",
+                "Tigris"
+            };
+
+            //gyümölcsök
+            string[] gyum = new string[] {
+                "Alma",
+                "Körte",
+                "Szőlő",
+                "Eper",
+                "Kivi",
+                "Cseresznye",
+                "Meggy",
+                "Szilva",
+                "Áfonya"
+            };
+
+            Random rnd = new Random();
+
+            int sor = 0;
+            //előfordulások megszámlálása
+            int midb = 0;
+            int madb = 0;
+            int fudb = 0;
+            int nyudb = 0;
+            int rgdb = 0;
+            int kadb = 0;
+            int zsedb = 0;
+            int bagdb = 0;
+            int tigdb = 0;
+
+
+            while (midb == 0 || madb == 0 || fudb == 0 || nyudb == 0 || rgdb == 0 || kadb == 0 ||
+                  zsedb == 0 || bagdb == 0 || tigdb == 0)
+            {
+                int vel1 = rnd.Next(0, szereplok.Length);//szereplők véletlen száma
+                int vel2 = rnd.Next(0, gyum.Length);//Gyümölcsök véletlen száma
+                if (vel1 == 0) midb++;
+                else if (vel1 == 1) madb++;
+                else if (vel1 == 2) fudb++;
+                else if (vel1 == 3) nyudb++;
+                else if (vel1 == 4) rgdb++;
+                else if (vel1 == 5) kadb++;
+                else if (vel1 == 6) zsedb++;
+                else if (vel1 == 7) bagdb++;
+                else if (vel1 == 8) tigdb++;
+                Console.WriteLine($"{sor + 1}. {szereplok[vel1]} szereti:{gyum[vel2]}");
+                kiMitSzeret.Add(szereplok[vel1] + " - " + gyum[vel2]);
+                sor++;
+            }
+            return kiMitSzeret;
+        }
 
         //Külső fájlból olvasás
         static List<string> SqlUtasitasok()
@@ -125,6 +204,7 @@ namespace MicimackoAdatbazis
             {
                 //  string sor = olvas.ReadLine().Split(':').ToString();
                 string[] sor = olvas.ReadToEnd().Split(':');
+                //foreach ( string s in sor ) Console.WriteLine(s);
                 //Console.WriteLine(sor.ToString());
                 for ( int i = 0; i < sor.Length; i++)
                 {
@@ -132,6 +212,7 @@ namespace MicimackoAdatbazis
                 }
                 //sqlUtasitasok.Add(sor);
             }
+            Console.WriteLine("első utasítás: " + sqlUtasitasok[0]);
             olvas.Close();
             return sqlUtasitasok;
 
--- MicimackoAdatbazis/utasitasok.sql
+++ MicimackoAdatbazis/utasitasok.sql
@@ -1,27 +1,45 @@
-﻿CREATE TABLE  IF NOT EXISTS Szereplok
-(SzereploAz INTEGER PRIMARY KEY AUTOINCREMENT,
-Nev VARCHAR(30))
+﻿CREATE TABLE  IF NOT EXISTS `Szereplok`
+(`SzereploAz` INTEGER PRIMARY KEY AUTOINCREMENT,
+`Nev` VARCHAR(30)
+)
 ;
 :
 
-CREATE TABLE  IF NOT EXISTS Gyumolcsok
-(GyumolcsAz INTEGER PRIMARY KEY AUTOINCREMENT,
-Nev VARCHAR(30))
+CREATE TABLE  IF NOT EXISTS `Gyumolcsok`
+(`GyumolcsAz` INTEGER PRIMARY KEY AUTOINCREMENT,
+`Nev` VARCHAR(30)
+)
 ;
 :
 
-CREATE TABLE  IF NOT EXISTS SzereplokGyumolcsok
-(SzereploAz INTEGER ,
-GyumolcsAz INTEGER,
-PRIMARY KEY(SzereploAz,GyumolcsAz),
-FOREIGN KEY (SzereploAz) REFERENCES Szereplok(SzereploAz),
-FOREIGN KEY (GyumolcsAz) REFERENCES Gyumolcsok(GyumolcsAz))
-;
-:
-INSERT INTO Szereplok(Nev) VALUES ("Micimackó")
+CREATE TABLE  IF NOT EXISTS `SzereplokGyumolcsok`
+(`SzereploAz` INTEGER ,
+`GyumolcsAz` INTEGER,
+PRIMARY KEY(`SzereploAz`,`GyumolcsAz`),
+FOREIGN KEY (`SzereploAz`) REFERENCES `Szereplok`(`SzereploAz`),
+FOREIGN KEY (`GyumolcsAz`) REFERENCES `Gyumolcsok`(`GyumolcsAz`))
 ;
 :
 
-INSERT INTO Szereplok(Nev) VALUES ("Malacka")
+INSERT INTO `Szereplok`(`Nev`) VALUES 
+('Micimackó'
+)
 ;
+:
+INSERT INTO `Szereplok`(`Nev`) VALUES('Malacka');
+:
+
+INSERT INTO `Szereplok`(`Nev`) VALUES('Füles');
+:
+INSERT INTO `Szereplok`(`Nev`) VALUES('Nyuszi');
+:
+INSERT INTO `Szereplok`(`Nev`) VALUES('Róbert Gida');
+:
+INSERT INTO `Szereplok`(`Nev`) VALUES('Kanga');
+:
+INSERT INTO `Szereplok`(`Nev`) VALUES('Zsebibaba');
+:
+INSERT INTO `Szereplok`(`Nev`) VALUES('Bagoly');
+:
+INSERT INTO `Szereplok`(`Nev`) VALUES('Tigris');
 :
